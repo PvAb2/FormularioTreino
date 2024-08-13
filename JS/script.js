8ce53338d7d7217d988d5040dc8fd820eb79d138
PvAb2 <estudanteifrocacoal@gmail.com>
2024-08-13 16:54:33 -0400

    IntroducaoJS

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+alert('Olá mundo');
+document.write('Escrevendo na página<br>');
+document.write('Mais uma linha');
+
+console.log('mensagem do desenvolvedor');
+
+document.getElementById('elemento').innerHTML = "André";
+
+var nome ='Pedro';
+var ano = 2002;
+var profissao='Professor'
+
+document.write('<br> Meu nome ' + nome+ ' Sou '+ profissao+ ' desde ' + ano);
+var n = prompt('Digite seu nome ');
+document.write('<p>Seja bem vindo '+n+'</p>');
+
+var n1 = parseInt(prompt('Digite o 1° número'));
+var n2 = parseInt(prompt('Digite o 2° número'));
+
+document.write('<br>');
+document.write(n1+n2);
+
+function somar(){
+    
+}
